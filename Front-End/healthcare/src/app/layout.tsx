d68a6a73d34450839095307f33cf7fe4310c9b09
AshanDhanush <ashandhanushka16@gmail.com>
2026-05-08 16:58:09 +0530

    Fixed the errors in the Layout.tsx

--- Front-End/healthcare/src/app/layout.tsx
+++ Front-End/healthcare/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<<<<<<< Updated upstream
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-<<<<<<< HEAD
-=======
 import { AuthProvider } from "@/context/AuthContext";
->>>>>>> Stashed changes
-=======
-import { AuthProvider } from "@/context/AuthContext";
->>>>>>> a152ae2f5bc4ad09727051a093c360dda90b5331
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,27 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
       <body className="min-h-full flex flex-col bg-slate-50">
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
         <AuthProvider>
->>>>>>> a152ae2f5bc4ad09727051a093c360dda90b5331
-        <Navbar />
-        <main className="flex-grow">
-          {children}
-        </main>
-        <Footer />
-<<<<<<< HEAD
-=======
-        <AuthProvider>
-        <main className="flex-grow">
-          {children}
-        </main>
+          <main className="flex-grow">
+            {children}
+          </main>
         </AuthProvider>
->>>>>>> Stashed changes
-=======
-        </AuthProvider>
->>>>>>> a152ae2f5bc4ad09727051a093c360dda90b5331
       </body>
     </html>
   );
